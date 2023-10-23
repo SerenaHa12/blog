313c06e326650afc2d257b1a0232540d7c480de8
SerenaHa12 <serenavander.ha@gmail.com>
2023-10-23 11:19:07 +0700

    connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,27 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+console.log(process.env.MONGODB_URI);
+const app = express();
 
-app.use(cors());
-app.use(cookieParser());
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+app.use(cors()); // cor dùng để ngăn chặn cross origin
+app.use(cookieParser()); // dùng để tạo cookie và gán cookie
 app.use(express.json());
 
-const app = express();
 app.listen(5500, () => {
   console.log("listening on port");
 });
